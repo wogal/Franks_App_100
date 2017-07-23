c4b33ba5a78a3ca58322814abacfe746ec868f9b
wogal <egilgeous@gmail.com>
2017-07-23 18:20:30 -0400

    // Android Template With FaceBook And Sd Card Intergration // Branch_100 // Branch_101 .. adding select location

---
+++ app/src/androidTest/java/Location_RecyclerView_Helpers/Location_RecyclerViewcode.java
@@ -0,0 +1,10 @@
+package Location_RecyclerView_Helpers;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wogal on 7/23/2017.
+ */
+
+public class Location_RecyclerViewcode  extends AppCompatActivity {
+}
