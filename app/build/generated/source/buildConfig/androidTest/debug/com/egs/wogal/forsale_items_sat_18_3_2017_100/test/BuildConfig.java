05ddd22dd2469805699b97f0dac1a21ced9f17a5
wogal <egilgeous@gmail.com>
2017-07-23 18:20:03 -0400

    // Android Template With FaceBook And Sd Card Intergration // Branch_100 // Branch_101 .. adding select location

--- app/build/generated/source/buildConfig/androidTest/debug/com/egs/wogal/forsale_items_sat_18_3_2017_100/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/egs/wogal/forsale_items_sat_18_3_2017_100/test/BuildConfig.java
@@ -5,7 +5,7 @@ package com.egs.wogal.forsale_items_sat_18_3_2017_100.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.egs.wogal.forsale_items_sat_18_3_2017_100.test";
+  public static final String APPLICATION_ID = "com.egs.wogal.forsale_items_sat_18_3_2017_100.Location_RecyclerViewcode";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
