0f94d63ee971b7b63fa05c6ea2cfba810f3107cb
wogal <egilgeous@gmail.com>
2017-08-07 20:58:07 -0400

    // Android Template With FaceBook And Sd Card Intergration // Branch_100 // Branch_101 .. adding select location

--- app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Location_Helpers/MySql_LocationHelper.java
+++ app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Location_Helpers/MySql_LocationHelper.java
@@ -8,7 +8,14 @@ package com.egs.wogal.forsale_items_sat_18_3_2017_100.Location_Helpers;
 public class MySql_LocationHelper {
     //  MySQLConnection
 
+    private static final String url = "";
+    private static final String user = "";
+    private static final String pass = "";
+
+
     public void testDb () {
+
+
 
 
     }
