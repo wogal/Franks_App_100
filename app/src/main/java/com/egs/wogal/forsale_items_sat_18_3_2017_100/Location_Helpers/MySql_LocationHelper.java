46fdb0b33934e4eecef376e7429ad130f715d498
wogal <egilgeous@gmail.com>
2017-08-09 09:46:09 -0400

    // Android Template With FaceBook And Sd Card Intergration // Branch_100 // Branch_101 .. adding select location

--- app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Location_Helpers/MySql_LocationHelper.java
+++ app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Location_Helpers/MySql_LocationHelper.java
@@ -1,8 +1,17 @@
 package com.egs.wogal.forsale_items_sat_18_3_2017_100.Location_Helpers;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 /**
  * Created by wogal on 8/7/2017.
@@ -11,34 +20,50 @@ import java.sql.DriverManager;
 public class MySql_LocationHelper {
     //  MySQLConnection
 
-    private static final String url = "jdbc:mysql:johnny.heliohost.org";
-    private static final String user = "wogal";
-    private static final String pass = "adv_51370_sql_server";
+    private static final String _url = "http://www.johnny.heliohost.org";
+    private static final String _user = "wogal";
+    private static final String _pass = "adv_51370_sql_server";
     private String str;
 
     public boolean testDb () {
-        try{
-
-            Class.forName( "com.mysql.jdbc.Driver" ).newInstance();
-
-            Connection myCon = DriverManager.getConnection( url,user,pass );
-
-            myCon.close();
-
-
-       //     Connection con = DriverManager.getConnection( url,user,pass );
-       //     String result = "Data Base Connected ";
-        //    Statement st = con.createStatement();
-            //    ResultSet rs = st.executeQuery(  )
 
 
 
+   //     String link = "http://johnny.heliohost.org/login.php?username="+username+"& password="+password;
 
-        }catch (Exception e){
-            str = e.getMessage();
-
-    return false;
+        try {
+            String user_name = _user;
+            String password = _pass;
+            URL url = new URL(_url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setDoInput(true);
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+            String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
+                    +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+            bufferedWriter.write(post_data);
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            outputStream.close();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+            String result="";
+            String line="";
+            while((line = bufferedReader.readLine())!= null) {
+                result += line;
+            }
+            bufferedReader.close();
+            inputStream.close();
+            httpURLConnection.disconnect();
+            return false;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
 
 
         return true;
